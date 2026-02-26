6397816f2cf5ce56029051f6afde14ca64e338a5
Ziad Fathy <66222287+ziadfathy7@users.noreply.github.com>
2026-02-27 00:09:39 +0200

    Part 1  Q4

--- AssignmentOOP01/Program.cs
+++ AssignmentOOP01/Program.cs
@@ -1,10 +1,78 @@
 ﻿namespace AssignmentOOP01
 {
+
+    #region Part01
+    #region Q1
+
+    //class Student
+    //{
+    //    public int Id;
+    //    public int age;
+    //    // Can use Inheritance
+
+    //}
+
+    //struct Color
+    //{
+    //    // Cant use inheritance
+    //    public string Name;
+    //}
+
+    #endregion
+
+    #region Q2
+    //class Person
+    //{
+    //    public int Id; // i can access from any file or project 
+    //    private string? passowrd; // cant access outside this scope only
+    //}
+    #endregion
+
+    #region Q3
+    /* 1-Create a Class Library Project
+     * 2-Add a Class Inside the Library
+     * 3-Build the Library
+     * 4-Reference the Class Library in Another Project
+     * 5-Use the Library
+     
+     */
+
+    #endregion
+
+    #region Q4
+    /*
+        Class Library is project contain reusable code to use in another projects to prevent code repeat
+     */
+    #endregion
+    #endregion
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //// Class
+            //Student s1 = new Student();
+            //Student s2 = new Student();
+
+            //s1.age = 24;
+            //s2 = s1;
+
+            //Console.WriteLine(s2.age);
+
+            //// Same age because theyre poinitng to same object in heap
+            //// I have to make object to work with class
+
+
+            //// Struct
+            //Color C1;
+            //C1.Name = "Green";
+            //Color C2;
+            //C2 = C1;
+            //C2.Name = "Red";
+            //Console.WriteLine(C1.Name);
+            //// Still Green because it only copies data not the object
         }
     }
 }
